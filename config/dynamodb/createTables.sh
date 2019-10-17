2777e5532e2ded48bff0ffc09893fbd42f561c9f
sebestindragos <sebestin.dragos@gmail.com>
2019-10-17 21:46:19 +0300

    Fix dynamo db script.

--- config/dynamodb/createTables.sh
+++ config/dynamodb/createTables.sh
@@ -1,1 +1,1 @@
-aws dynamodb create-table --cli-input-json ./tables.json --endpoint-url http://localhost:8000
+aws dynamodb create-table --cli-input-json file://./config/dynamodb/tables.json --endpoint-url http://localhost:8000
