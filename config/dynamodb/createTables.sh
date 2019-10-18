7b4eaaba660c847c593e8004fdbfc7591d47f14d
sebestindragos <sebestin.dragos@gmail.com>
2019-10-18 17:53:16 +0300

    [master] Add dynamo config for politicians table.

--- config/dynamodb/createTables.sh
+++ config/dynamodb/createTables.sh
@@ -1,1 +1,2 @@
-aws dynamodb create-table --cli-input-json file://./config/dynamodb/tables.json --endpoint-url http://localhost:8000
+aws dynamodb create-table --cli-input-json file://./config/dynamodb/users-table.json --endpoint-url http://localhost:8000
+aws dynamodb create-table --cli-input-json file://./config/dynamodb/politicians-table.json --endpoint-url http://localhost:8000
